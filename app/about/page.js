bad2c84ad7204885381c8dff4ea1b71e40c82f85
kubirthan <kubirthan7991@gmail.com>
2024-08-31 21:51:59 +0530

    creating about page

---
+++ app/about/page.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main class="container mx-auto px-4 py-6">
+      <h2 class="text-4xl font-bold mb-4">About Us</h2>
+      <p>
+        This is free platform fro learning coding & build projects easily.
+      </p>
+    </main>
+  );
+}
