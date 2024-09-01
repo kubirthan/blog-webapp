faee3cc76c934b5d1c70bec02288c8b221508426
kubirthan <kubirthan7991@gmail.com>
2024-09-01 22:07:17 +0530

    Changing date format

--- models/postModel.js
+++ models/postModel.js
@@ -3,12 +3,24 @@ import { model, Schema, models } from "mongoose";
 const postSchema = new Schema({
     title: String,
     description: String,
-    image: String
+    image: String,
+    created_at:String
 }, { toJSON: {virtuals: true}})
 
 postSchema.virtual('short_description').get(function(){
     return this.description.substr(0,50)+'...'
 })
+
+postSchema.virtual('created_at_format').get(function(){
+    return changeDateFormat(this.created_at)
+})
+
+function changeDateFormat(date_str){
+    const date = new Date(date_str)
+    const months = ["January","february","March","April","May","June","July","August","Sepetmber","october","november","December"]
+
+    return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+}
 
 const postModel = models.Post ||  model('Post', postSchema)
 
