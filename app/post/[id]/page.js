7695d39ead610ccb186bf79fbb6750ba021cdc2c
kubirthan <kubirthan7991@gmail.com>
2024-09-01 22:36:50 +0530

    Showing post details using API

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,13 +1,30 @@
-export default function Post() {
-  return (
-    <main className="container mx-auto px-4 py-6">
-      <h2 className="text-4xl font-bold mb-4">Blog Post Title</h2>
-      <p className="text-gray-500">Published on January 1, 2022</p>
-      <img src="https://picsum.photos/200" alt="Post Image" className="my-4" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    </main>
-  );
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Post({ params }) {
+  const id = params.id;
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/post/" + id)
+      .then((res) => res.json())
+      .then((res) => setPost(res));
+  }, []);
+
+  return <>
+      {post && <main className="container mx-auto px-4 py-6">
+        <h2 className="text-4xl font-bold mb-4">{post.title}</h2>
+        <p className="text-gray-500">Published on {post.created_at_format}</p>
+        <img
+          src={post.image}
+          alt="Post Image"
+          className="my-4"
+        />
+        <p>
+          {post.description}
+        </p>
+      </main>}
+    </>
+  
 }
