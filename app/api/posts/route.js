84d93771fdf1c963668b8b7d8696186936500de1
kubirthan <kubirthan7991@gmail.com>
2024-09-02 16:01:50 +0530

    Implementing post search

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,10 +1,22 @@
 import PostModel from "../../../models/postModel";
 import connectMongo from "../../../utils/connectMongo";
 
-export async function GET() {
+export async function GET(req) {
+  const query = req.nextUrl.searchParams.get('q')
+  
   try {
     await connectMongo();
-    const postData = await PostModel.find({});
+    let postData
+    if(query){
+      postData = await PostModel.find({
+        $or : [
+          {title: new RegExp(query,'i')}
+        ]
+      })
+    }else {
+      postData = await PostModel.find({});
+    }
+    
     return Response.json(postData);
   } catch (error) {
     return Response.json({ message: error.message });
--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,31 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-
   const [posts, setPosts] = useState([])
+  const inputRef = useRef("")
+  const [search, setSearch] = useState(false)
 
   useEffect(()=> {
     fetch(process.env.NEXT_PUBLIC_API_URL+'/posts')
     .then((res) => res.json())
     .then(res => setPosts(res))
   }, [])
+
+  const searchPost = (e) => {
+    if(e.type == 'keydown' && e.key !== 'Enter'){
+      return
+    }
+
+    setSearch(true)
+     fetch(process.env.NEXT_PUBLIC_API_URL+'/posts?q='+inputRef.current.value)
+    .then((res) => res.json())
+    .then(res => setPosts(res))
+    .finally(() => setSearch(false))
+    
+  }
 
   return (
     <>
@@ -24,8 +38,8 @@ export default function Home() {
       </p>
     </main>
     <div className="flex justify-end px-4">
-    <input type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
-    <button className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">Search</button>
+    <input onKeyDown={searchPost}  ref={inputRef} type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
+    <button onClick={searchPost} disabled={search} className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">{search?'...':'Search'}</button>
   </div>
   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post) => (
@@ -36,6 +50,8 @@ export default function Home() {
           <p className="text-gray-600">{post.short_description}</p>
         </div>
         </Link>))}
+        {!posts.length > 0 && inputRef.current.value && <p>No posts available for this query:{inputRef.current.value}</p>}
+        
     </div>
   </>
   );
